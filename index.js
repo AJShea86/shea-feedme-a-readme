1cff7dda4d9c4edab074eca95209b1c167b873b4
Andrew S <andrewsheamls@gmail.com>
2022-03-02 17:09:33 -0700

    created prompts to gather user info

---
+++ index.js
@@ -0,0 +1,55 @@
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of the project?",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "Enter a description of the project.",
+      name: "projectDescription",
+    },
+    {
+      type: "input",
+      message: "Enter instructions for installation.",
+      name: "projectInstall",
+    },
+    {
+      type: "input",
+      message: "Enter usage information.",
+      name: "projectInformation",
+    },
+    {
+      type: "input",
+      message: "Enter contribution guidelines.",
+      name: "projectGuidelines",
+    },
+    {
+      type: "input",
+      message: "Enter test instructions.",
+      name: "projectInstructions",
+    },
+    {
+      type: "checkbox",
+      message: "Choose a license for the application.",
+      choices: ["License 1", "License 2", "License 3", "License 4"],
+      name: "projectLicenseChoice",
+    },
+    {
+      type: "input",
+      message: "Enter your Github username.",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "Enter your email.",
+      name: "email",
+    },
+  ])
+
+  .then((data) => {
+      console.log(data);
+});
